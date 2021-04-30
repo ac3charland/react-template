8e278599c512b579e2b395a3dcbaa4a47765181e
Alex Charland <ac3charland@gmail.com>
2021-04-30 07:23:52 -0500

    Break index.js into an app.jsx component to better handle top-level network calls & listeners

--- src/index.js
+++ src/index.js
@@ -1,36 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import {Provider} from 'react-redux'
+import App from './app'
 import './index.scss'
 import * as serviceWorker from './serviceWorker'
-import {Route, BrowserRouter as Router, Switch} from 'react-router-dom'
-import NavBar from './components/nav-bar/nav-bar'
-import Footer from './components/footer/footer'
-import HomePage from './pages/home-page/home-page'
-import SecondaryPage from './pages/secondary-page/secondary-page'
-import {NAVBAR_HEIGHT, SECONDARY_PAGE_URL} from './utils/constants'
+
 import makeStore from './store'
 
 const store = makeStore()
 
 ReactDOM.render(
   <Provider store={store}>
-    <React.StrictMode>
-      <Router>
-        <div className='content-wrapper' style={{minHeight: `calc(100vh - ${NAVBAR_HEIGHT + 2}rem)`}}>
-          <NavBar />
-          <div className='flex' style={{minHeight: `calc(100vh - ${NAVBAR_HEIGHT + 2}rem)`, marginTop: `${NAVBAR_HEIGHT + 2}rem`}}>
-            <div className='page-wrapper'>
-              <Switch>
-                <Route exact path='/' component={HomePage} />
-                <Route exact path={SECONDARY_PAGE_URL} component={SecondaryPage} />
-              </Switch>
-            </div>
-            <Footer />
-          </div>
-        </div>
-      </Router>
-    </React.StrictMode>
+    <App/>
   </Provider>,
   document.getElementById('root')
 )
